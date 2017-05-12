5026cf3e763cf15dab0f48c63535805d349258e7
luixlacrux <luixlacrux18@gmail.com>
2017-05-12 01:20:26 -0400

    Update ordering files assets

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const app = express()
 
+app.set('view engine', 'pug')
+
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
-  res.send('hello world!!')
+  res.render('index')
 })
 
 app.listen(3000, (err) => {
